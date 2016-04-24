ddf3a1f989cc2618bbd8e06a5e0dc6f25387115f
aw449 <awong7905@gmail.com>
2016-04-24 06:19:03 -0400

    net code

--- Win32Project3/Win32Project3/Win32Project3.cpp
+++ Win32Project3/Win32Project3/Win32Project3.cpp
@@ -46,6 +46,12 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	int       nCmdShow)
 {
 	ip[0] = 0;
+	HACCEL hAccelTable;
+	int iResult; //To take the result of function calls
+	WSADATA wsaData; //Sockets.
+	struct addrinfo *result = NULL, //For socketing address purposes
+		*ptr = NULL,
+		hints;
 
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
@@ -88,11 +94,69 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	UpdateWindow(hWnd);
 
 	MSG msg;
+	int connected = 0;
 
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+
+		//First time connection setup
+		if (ip[0] != 0 && !connected) {
+			iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+			if (iResult != 0) {
+				printf("WSAStartup failed with error: %d\n", iResult);
+				return 1;
+			}
+
+			ZeroMemory(&hints, sizeof(hints)); // A calloc
+			hints.ai_family = AF_UNSPEC;
+			hints.ai_socktype = SOCK_STREAM;
+			hints.ai_protocol = IPPROTO_TCP;
+
+			// Resolve the server address and port
+			iResult = getaddrinfo("192.168.1.113", DEFAULT_PORT, &hints, &result);
+			if (iResult != 0) {
+				printf("getaddrinfo failed with error: %d\n", iResult);
+				WSACleanup();
+				return 1;
+			}
+			// Attempt to connect to an address until one succeeds
+			for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+				// Create a SOCKET for connecting to server
+				ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+					ptr->ai_protocol);
+				if (ConnectSocket == INVALID_SOCKET) {
+					printf("socket failed with error: %ld\n", WSAGetLastError());
+					WSACleanup();
+					return 1;
+				}
+
+				// Connect to server.
+				iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+				if (iResult == SOCKET_ERROR) {
+					closesocket(ConnectSocket);
+					ConnectSocket = INVALID_SOCKET;
+					continue;
+				}
+				printf("Connected to socket at IP: 192.168.1.113");
+				connected = 1;
+				break;
+			}
+
+			freeaddrinfo(result);
+
+			if (ConnectSocket == INVALID_SOCKET) {
+				printf("Unable to connect to server!\n");
+				WSACleanup();
+				return 1;
+			}
+
+		}
+		//We are connected send the byte stream
+		else if (connected == 1) {
+			break;
+		}
 	}
 
 	
